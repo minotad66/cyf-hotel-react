59df776c6d7e642523885e7723fe4e0cea08171f
Darwin <50497333+minotad66@users.noreply.github.com>
2019-09-04 10:03:35 -0500

    actualizacion

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,46 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import Moment from "moment";
 
-const SearchResults = props => {
-  const tableData = props.results;
-  const newTableData = tableData.map(infoData => (
-    <tr>
-      <th scope="row">{infoData.id}</th>
-      <td>{infoData.title}</td>
-      <td>{infoData.firstName}</td>
-      <td>{infoData.surname}</td>
-      <td>{infoData.email}</td>
-      <td>{infoData.roomId}</td>
-      <td>{infoData.checkInDate}</td>
-      <td>{infoData.checkOutDate}</td>
-      <td>
-        {Moment(infoData.checkOutDate).diff(infoData.checkInDate, "days")}
-      </td>
-    </tr>
-  ));
+let i = 0;
 
-  const handleRearchResults = tr => {
-    console.log("Hello, welcome to react");
+class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgColor: ""
+    };
+  }
+
+  handleClick = e => {
+    if (i % 2 !== 0) {
+      this.setState({ bgColor: "" });
+    } else {
+      this.setState({ bgColor: "red" });
+    }
+
+    i++;
   };
 
-  return (
-    <table className="table table-striped">
-      <thead>
-        <tr onClick={handleRearchResults(this)}>
-          <th scope="col" />
-          <th scope="col">Title</th>
-          <th scope="col">FirstName</th>
-          <th scope="col">LastName</th>
-          <th scope="col">Email</th>
-          <th scope="col">RoomId</th>
-          <th scope="col">CheckInDate</th>
-          <th scope="col">CheckOutDate</th>
-          <th scope="col">Days</th>
-        </tr>
-      </thead>
-      <tbody>{newTableData}</tbody>
-    </table>
-  );
-};
+  render() {
+    let tableData = this.props.results;
+    let newTableData = tableData.map(infoData => (
+      <tr
+        onClick={this.handleClick}
+        style={{ backgroundColor: this.state.bgColor }}
+      >
+        <th scope="row">{infoData.id}</th>
+        <td>{infoData.title}</td>
+        <td>{infoData.firstName}</td>
+        <td>{infoData.surname}</td>
+        <td>{infoData.email}</td>
+        <td>{infoData.roomId}</td>
+        <td>{infoData.checkInDate}</td>
+        <td>{infoData.checkOutDate}</td>
+        <td>
+          {Moment(infoData.checkOutDate).diff(infoData.checkInDate, "days")}
+        </td>
+      </tr>
+    ));
+
+    return (
+      <table className="table table-striped">
+        <thead>
+          <tr
+            onClick={this.handleClick}
+            style={{ backgroundColor: this.state.bgColor }}
+          >
+            <th scope="col" />
+            <th scope="col">Title</th>
+            <th scope="col">FirstName</th>
+            <th scope="col">LastName</th>
+            <th scope="col">Email</th>
+            <th scope="col">RoomId</th>
+            <th scope="col">CheckInDate</th>
+            <th scope="col">CheckOutDate</th>
+            <th scope="col">Days</th>
+          </tr>
+        </thead>
+        <tbody>{newTableData}</tbody>
+      </table>
+    );
+  }
+}
 
 export default SearchResults;
